f0a9e95d753bd77e953c8474394addb8a0a9a541
Bayego <edubayego@gmail.com>
2023-11-20 12:24:12 +0100

    main script
    Uploaded the main code, that deletes all junk files and directories in temp and prefetch

--- clean.py
+++ clean.py
@@ -1,7 +1,19 @@
-import os  
+import os , shutil
 
 userPath = os.path.expanduser("~")
-paths = [userPath + "/AppData/Local/Temp", userPath + "/Desktop/UOC"]
-print(paths)
-print (os.listdir(paths[1]))
-#print (os.path.expanduser("~"))
+paths = [userPath + "/AppData/Local/Temp", "C:/Windows/Prefetch", userPath + "/temp"]
+
+for path in paths:
+    try:
+        for file in os.listdir(path):
+            filePath = os.path.join(path, file)
+            try:
+                if os.path.isfile(filePath):
+                    os.remove(filePath)
+                elif os.path.isdir(filePath):
+                    os.chmod(filePath, 0o777)
+                    shutil.rmtree(filePath)
+            except Exception as e:
+                print(f"Couldn't delete {filePath}: {e}")
+    except Exception as e:
+        print(f"Error deleting content from {path}: {e}") 
